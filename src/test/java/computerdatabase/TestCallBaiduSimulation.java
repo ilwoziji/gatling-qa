d9a929fc9ad84f46097a166ae23ea72542a080b8
ilwoziji@126.com <ilwoziji@126.com>
2023-07-12 10:30:40 +0800

    zzz

--- src/test/java/computerdatabase/TestCallBaiduSimulation.java
+++ src/test/java/computerdatabase/TestCallBaiduSimulation.java
@@ -29,9 +29,9 @@ import java.util.concurrent.ThreadLocalRandom;
  */
 public class TestCallBaiduSimulation extends Simulation {
 
-    private static int TEST_USER_COUNT = 10000;
+    private static int TEST_USER_COUNT = 3000;
 
-    private static int DURATION_SECONDS = 500;
+    private static int DURATION_SECONDS = 10;
 
     private HttpProtocolBuilder httpProtocol = http
             //.baseUrl("http://localhost:18000")
