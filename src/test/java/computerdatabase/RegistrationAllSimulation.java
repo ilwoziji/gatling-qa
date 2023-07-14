dadf99533b464c2470baf6c60fe2923daaaf92c4
ilwoziji@126.com <ilwoziji@126.com>
2023-07-14 17:51:41 +0800

    --

--- src/test/java/computerdatabase/RegistrationAllSimulation.java
+++ src/test/java/computerdatabase/RegistrationAllSimulation.java
@@ -18,13 +18,13 @@ import java.util.UUID;
  */
 public class RegistrationAllSimulation extends Simulation {
 
-    private static int TEST_USER_COUNT = 6000;
+    private static int TEST_USER_COUNT = 10;
 
-    private static int DURATION_SECONDS = 100;
+    private static int DURATION_SECONDS = 10;
 
     private HttpProtocolBuilder httpProtocol = http
-            .baseUrl("http://172.31.18.132:10000")
-            //.baseUrl("https://pre.actqa.com")
+            //.baseUrl("http://172.31.18.132:10000")
+            .baseUrl("https://pre.actqa.com")
             .inferHtmlResources()
             .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36")
             .shareConnections();
@@ -51,7 +51,7 @@ public class RegistrationAllSimulation extends Simulation {
         setUp(scn.injectOpen(constantUsersPerSec(TEST_USER_COUNT).during(DURATION_SECONDS))).protocols(httpProtocol);
     }
 
-//    public static void main(String[] args) {
-//        System.out.println(generateBody());
-//    }
+    public static void main(String[] args) {
+        System.out.println(generateBody());
+    }
 }
