d328184f727f03b0762bce174679fcaa448957aa
ilwoziji@126.com <ilwoziji@126.com>
2023-07-18 16:59:57 +0800

    --

--- src/test/java/computerdatabase/RegistrationAllSimulation.java
+++ src/test/java/computerdatabase/RegistrationAllSimulation.java
@@ -23,7 +23,7 @@ public class RegistrationAllSimulation extends Simulation {
     private static int DURATION_SECONDS = 100;
 
     private HttpProtocolBuilder httpProtocol = http
-            .baseUrl("http://172.31.37.21:8000")
+            .baseUrl("http://172.31.6.27:8000")
             //.baseUrl("https://pre.actqa.com")
             .inferHtmlResources()
             .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36")
